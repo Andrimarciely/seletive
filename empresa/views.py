1484925f2d5f8c8dc6d38552963a328899b43b7b
Andrimarciely Souza <andrimarciely@gmail.com>
2022-11-10 18:56:44 -0400

    listagem das empresas

--- empresa/views.py
+++ empresa/views.py
@@ -45,3 +45,7 @@ def nova_empresa(request):
         empresa.save()
         messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
         return redirect('/home/nova_empresa')
+
+def empresas(request):
+    empresas = Empresa.objects.all()
+    return render(request, 'empresa.html', {'empresas':empresas})
