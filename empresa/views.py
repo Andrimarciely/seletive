30f590e10342a49c44b38302a8ef8ab9f3850011
Andrimarciely Souza <andrimarciely@gmail.com>
2022-11-13 11:22:24 -0400

    new feat - filtro de empresas por nome e tecnologia

--- empresa/views.py
+++ empresa/views.py
@@ -45,9 +45,17 @@ def nova_empresa(request):
         empresa.save()
         messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
         return redirect('/home/nova_empresa')
+        return redirect('/home/nova_empresa')
 
 def empresas(request):
+    tecnologias_filtrar = request.GET.get('tecnologias')
+    nome_filtrar = request.GET.get('nome')
     empresas = Empresa.objects.all()
+    if tecnologias_filtrar:
+        empresas = empresas.filter(tecnologias = tecnologias_filtrar)
+    if nome_filtrar:
+        empresas = empresas.filter(nome__icontains=nome_filtrar)
+
     tecnologias = Tecnologias.objects.all()
     return render(request, 'empresa.html', {'empresas': empresas, 'tecnologias': tecnologias})
 
